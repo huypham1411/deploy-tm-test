1a62975846cbdf7350e7b97c95dcc42d1144803d
huypham1411 <54835897+huypham1411@users.noreply.github.com>
2020-06-16 20:53:03 +0700

    test n+1

--- client/src/components/General/Nav.js
+++ client/src/components/General/Nav.js
@@ -1,49 +1,49 @@
-import React,{useState} from 'react';
-import '../../styles/components/General/Nav.css';
-import { NavLink } from 'react-router-dom';
-import {useDispatch} from 'react-redux';
-import cart from '../../assets/cardIcon/cart.png';
-import { Link,useHistory } from 'react-router-dom';
-// import {sortChoose,filterChoose} from '../../action/sort-action';
-// import {pageReset} from '../../action/paginate';
-import {searching} from '../../action/search-field';
-import Axios from 'axios';
-// import Cart from '../General/Cart';
+import React, { useState } from "react";
+import "../../styles/components/General/Nav.css";
+import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import cart from "../../assets/cardIcon/cart.png";
+import { Link, useHistory } from "react-router-dom";
+// import { sortChoose, filterChoose } from "../../action/sort-action";
+// import { pageReset } from "../../action/paginate";
+import { searching } from "../../action/search-field";
+import Axios from "axios";
+// import Cart from "../General/Cart";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
-    // const [appear,setAppear]=useState(false)
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const [searchInput,setSearchInput]=useState('')
-    const styles = {
-        borderRadius: 10,
-        backgroundColor: '#00DD75'
-    }
-    //https://stackoverflow.com/questions/47879663/root-navlink-always-get-active-class-react-router-dom
-    const checkActive = (match, location) => {
-        //some additional logic to verify you are in the home URI
-        if (!location) return false;
-        const { pathname } = location;
-        // console.log(pathname);
-        return pathname === "/";
-    }
-    const getList=(e)=>{
-        if (e.key === 'Enter') {
-        const name=searchInput.toLowerCase();
-       Axios.get(`/products?name=${name}`)
-       .then(data=>{
-           const rSearchInput = name;
-           dispatch(searching(rSearchInput,data.data))
-           history.push('/Search');
-           //console.log('search',data.data);
-           setSearchInput('')
+  const [appear, setAppear] = useState(false);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [searchInput, setSearchInput] = useState("");
+  const styles = {
+    borderRadius: 10,
+    backgroundColor: "#00DD75",
+  };
+  //https://stackoverflow.com/questions/47879663/root-navlink-always-get-active-class-react-router-dom
+  const checkActive = (match, location) => {
+    //some additional logic to verify you are in the home URI
+    if (!location) return false;
+    const { pathname } = location;
+    // console.log(pathname);
+    return pathname === "/";
+  };
+  const getList = (e) => {
+    if (e.key === "Enter") {
+      const name = searchInput.toLowerCase();
+      Axios.get(`http://localhost:3030/products?name=${name}`)
+        .then((data) => {
+          const rSearchInput = name;
+          dispatch(searching(rSearchInput, data.data));
+          history.push("/Search");
+          //console.log('search',data.data);
+          setSearchInput("");
         })
-        .catch(err=>console.log(err));
-        }
+        .catch((err) => console.log(err));
     }
-    return (
+  };
+  return (
     <nav className="nav-bar navbar navbar-expand-lg">
       <button
         className="navbar-toggler"
@@ -59,22 +59,30 @@ const Nav = () => {
         </span>
       </button>
 
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul className="navbar-nav">
+        <li className="nav-item">
           <NavLink to="/" isActive={checkActive} activeStyle={styles}>
-            <li className="nav-item">Home</li>
+            Home
           </NavLink>
+        </li>
+        <li className="nav-item">
           <NavLink to="/Products" activeStyle={styles}>
-            <li className="nav-item">Our product</li>
+            Our product
           </NavLink>
+        </li>
+        <li className="nav-item">
           <NavLink to="/Policy" activeStyle={styles}>
-            <li className="nav-item">Policy</li>
+            Policy
           </NavLink>
+        </li>
+        <li className="nav-item">
           <NavLink to="/About" activeStyle={styles}>
-            <li className="nav-item">About us</li>
+            About us
           </NavLink>
+        </li>
         </ul>
-    </div>
+      </div>
 
       <div id="searchBar">
       <input type="search" id="searchInput" value={searchInput} onChange={(value)=>setSearchInput(value.target.value)} onKeyDown={e=>getList(e)}></input>
@@ -82,12 +90,12 @@ const Nav = () => {
       {/* <div className="img-container"><img src={cart} onClick={()=>{setAppear(!appear)}} alt="cart"></img></div>
             {appear&&<Cart/>}
         */}
-        <Link to="/cart">
+      <Link to="/cart">
         <div className="img-container">
           <img src={cart} alt="cart"></img>
         </div>
       </Link>
     </nav>
-    );
-}
+  );
+};
 export default Nav;
